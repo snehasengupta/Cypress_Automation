ff1fb46e2e4072a1cc23e23e4e61e168f096f8cf
sneha <bristisengupta027@gmail.com>
2024-05-13 00:19:46 +0530

    changesincode

--- cypress/integration/examples/test1.js
+++ cypress/integration/examples/test1.js
@@ -9,18 +9,28 @@ describe('My First Testsuite',()=>{ //describe is the test suite first arg is de
         cy.wait(2000);//put wait before validating
         cy.get('.product:visible').should('have.length',4);//should is a assertion method, by using visible tag after classname it will ommit the invisible elenments.
         //parent child chaining
-        cy.get('.products').find('.product').should('have.length',4);//find limits the scope
-        cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click(); // this line will add whatever present in 2nd index of the product array and on that position if it gets anything like add to cart it will perform click action.
-        //now remembering the problem statement next line will be dynamically selecting a specific product
+        cy.get('.products').as('productlocator');//this called alias where user optimizes the code by mentioning one resused classpath into a variable using as method and also for this type of assign method user dont have to manually handle any promises.
 
-        cy.get('.products').find('.product').each(($el, index,$list)=>{ //each method is like foreach in js which will iterate through each method, el will start from first index position element
+        cy.get('@productlocator').find('.product').should('have.length',4);//find limits the scope
+        cy.get('@productlocator').find('.product').eq(2).contains('ADD TO CART').click().then(()=>{
+            console.log('sneha')//as this simply js command to make it sychronyze neet to manually tackle this.
+        }); // this line will add whatever present in 2nd index of the product array and on that position if it gets anything like add to cart it will perform click action.
+        //now remembering the problem statement next line will be dynamically selecting a specific product
+        // using @ we can call the variable
+        cy.get('@productlocator').find('.product').each(($el, index,$list)=>{ //each method is like foreach in js which will iterate through each method, el will start from first index position element
         const veggiename=$el.find('h4.product-name').text() //it will get text from the first position and iterate till length
         if(veggiename.includes('Carrot'))
         {
         $el.find('button').click();//wrap is a promise method
         }
         })
-        cy.get('.brand').then((logo)=>{ //like this user can declare the step under the variable and put action methods on the variable
+        cy.get('.brand').as('logotext');
+        //method to assert
+        cy.get('@logotext').should('have.text','GREENKART');//should belongs to chai assertion so that it will automatically be taken care even though this is not a cypress method.
+        
+
+        //method to print in logs
+        cy.get('@logotext').then((logo)=>{ //like this user can declare the step under the variable and put action methods on the variable
             cy.log(logo.text())
 
 
---
+++ cypress/integration/examples/test2.js
@@ -0,0 +1,32 @@
+/// <reference types="Cypress" />
+
+describe('My Second Testsuite',()=>{
+    it('First Testcase',()=>{ 
+        cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
+        cy.get('.brand').as('logotext');
+        cy.get('@logotext').should('have.text','GREENKART');
+        cy.get('.search-keyword').type('ca');
+        cy.wait(2000);
+        cy.get('.products').as('productlocator');
+        cy.get('@productlocator').find('.product').each(($el, index, $list) => {
+ 
+            const textVeg=$el.find('h4.product-name').text()
+            if(textVeg.includes('Carrot'))
+            {
+            cy.wrap($el.find('button')).click()
+            }
+            })
+            cy.get('.cart-icon > img').click();
+            cy.contains('PROCEED TO CHECKOUT').click();
+            cy.get('[style="text-align: right; width: 100%; margin-top: 20px; margin-right: 10px;"] > :nth-child(14)').click();
+            cy.get('select').select('India');
+            cy.get('.chkAgree').check();
+            cy.get('button').contains('Proceed').click();
+            cy.get('[style="color:green;font-size:25px"]').as('OrderPlaced');
+            cy.get('@OrderPlaced').then((textdone)=>{
+                cy.log(textdone.text())
+            })
+            cy.get('@OrderPlaced').should('have.text','Thank you, your order has been placed successfully You\'ll be redirected to Home page shortly!!');
+        })
+
+    })
